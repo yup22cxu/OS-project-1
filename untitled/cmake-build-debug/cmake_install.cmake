0231553c94468138eee5e6fd2a96a394a25fe43a
yup22cxu <135335417+yup22cxu@users.noreply.github.com>
2023-12-04 21:00:34 +0000

    exit loop working

--- untitled/cmake-build-debug/cmake_install.cmake
+++ untitled/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/wizar/CLionProjects/untitled
+# Install script for directory: C:/Users/wizar/CLionProjects/OS-project-1-main/untitled
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/wizar/CLionProjects/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/wizar/CLionProjects/OS-project-1-main/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- untitled/main.c
+++ untitled/main.c
@@ -4,26 +4,21 @@
 
 
 int main() {
-    char userInput[50];
-    char exit[] = "exit";
+    char userInput[6];
+    char exit[6] = "exit\n";
+    char exit2[] = "";
     int length;
+    int x = 1;
+    char *token;
+    int y = 1;
     do {
         printf(">>>");
         fgets(userInput,50,stdin);
-        //scanf("%[^\n]s", userInput);
-        printf("%s", userInput);
-        char temp[] = userInput[50];
-        //puts(userInput);
-        /*for (int i = 0; i < 4; ++i) {
-            printf("%c\n", userInput[i]);
-        }
-        length = sizeof(userInput);
-        printf("%d", length);
-*/
 
+        token = strtok(userInput, " ");
 
     }
-    while (exit != userInput);
+    while (strcmp(exit,userInput) != 0);
     printf("Successfully exited");
 
 }
