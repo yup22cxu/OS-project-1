0708ecc0f88d38d57a1309722658e5d4a58928bc
yup22cxu <135335417+yup22cxu@users.noreply.github.com>
2023-12-05 19:22:16 +0000

    working processCommand and execCommand

--- untitled/main.c
+++ untitled/main.c
@@ -1,24 +1,89 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <limits.h>
 
 
+char currentDirectory[PATH_MAX] = "";
+
+void setcwd(){
+        if (getcwd(currentDirectory, sizeof(currentDirectory)) == NULL) {
+            perror("getcwd() error");
+        }
+};
+
+void changeCwd(char param1[]){
+    printf("param 1 %s\n", param1);
+
+}
+
+void execCommand(char userInput[]){
+    char* argv[100] = {};
+    char*token;
+    int i;
+
+    token = strtok(userInput, " ");
+    //processCommand(userInput);
+    while (token != NULL) {
+        printf("Token: %s\n", token);
+        // Pass NULL to get next token
+        argv[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    // add less then 100 parameters validation
+
+    char* envp[] = {NULL};
+    execve(argv[0], argv, envp);
+    exit(0);
+
+}
+
+void processCommand(char userInput[]) {
+
+    int cpid = fork();
+    if (cpid == 0) {
+        execCommand(userInput);
+    }
+    wait(&cpid);
+    char *command;
+    char *param1;
+    //setcwd();
+    //printf("this is the cwd %s\n", currentDirectory);
+    printf("%s\n", userInput);
+    command = strtok(userInput, " ");
+    param1 = strtok(NULL, " ");
+    printf("%s\n", command);
+    if (strcmp(command, "cd") == 0) {
+        printf("we made it\n");
+        changeCwd(param1);
+    }
+}
 
 int main() {
-    char userInput[6];
+    char userInput[] = "/bin/ls -al main.c";
     char exit[6] = "exit\n";
-    char exit2[] = "";
-    int length;
-    int x = 1;
-    char *token;
-    int y = 1;
+    processCommand(userInput);
+      //  system("ls");
+    //system("cat main.c");
+    return 0;
+
     do {
         printf(">>>");
-        fgets(userInput,50,stdin);
+        fgets(userInput,1000,stdin);
 
-        token = strtok(userInput, " ");
+        if (strcmp(exit,userInput) == 0) {
+            break;
+        }
+
+
 
     }
-    while (strcmp(exit,userInput) != 0);
+    while (1 == 1);
     printf("Successfully exited");
 
 }
+
+
